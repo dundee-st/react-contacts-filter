667d7f26b7982049d518dabb23f198d01e9300c8
dundee-st <dundee-st@users.noreply.github.com>
2020-10-05 15:43:38 +0300

    Updates

---
+++ precache-manifest.a7c877d3b4ad04c5122938d5767a537e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6eeb00cd9eb6d896cd4a886ccd85a6",
+    "url": "/wezom-test-contacts/index.html"
+  },
+  {
+    "revision": "00a9222c0099f47e7a38",
+    "url": "/wezom-test-contacts/static/css/main.b9962730.chunk.css"
+  },
+  {
+    "revision": "5578e3c1de6184326116",
+    "url": "/wezom-test-contacts/static/js/2.55f4aa21.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/wezom-test-contacts/static/js/2.55f4aa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a9222c0099f47e7a38",
+    "url": "/wezom-test-contacts/static/js/main.2f48a17e.chunk.js"
+  },
+  {
+    "revision": "a477f015829e37c8a0ff",
+    "url": "/wezom-test-contacts/static/js/runtime-main.0c56daf3.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/wezom-test-contacts/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wezom-test-contacts/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/wezom-test-contacts/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/wezom-test-contacts/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
